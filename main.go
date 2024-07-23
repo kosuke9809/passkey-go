110f1a901523fd68b75b2d0ac3d896da3fe778b6
kosuke9809 <uht0912k@outlook.com>
2024-07-23 23:58:11 +0900

    add: html& router

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"passkey-auth/router"
+)
 
+func main() {
+	router, err := router.New()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Server started on port 8082")
+	log.Fatal(router.Start(":8082"))
 }
--- router/router.go
+++ router/router.go
@@ -1,7 +1,33 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+	"passkey-auth/auth"
+)
 
 type Router struct {
 	http.ServeMux
+}
+
+func New() (*Router, error) {
+	err := auth.InitWebAuthn()
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Router{}
+	s.HandleFunc("/register/begin", auth.BeginRegistration)
+	s.HandleFunc("/register/finish", auth.FinishRegistration)
+	s.HandleFunc("/login/begin", auth.BeginLogin)
+	s.HandleFunc("/login/finish", auth.FinishLogin)
+	s.HandleFunc("/", serveHTML)
+	return s, nil
+}
+
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func (s *Router) Start(addr string) error {
+	return http.ListenAndServe(addr, s)
 }
